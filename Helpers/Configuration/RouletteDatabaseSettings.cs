d9eaa6bc9295f95425fcc3d6228f13ece1c4fb79
Ospina Lopez, Gustavo Adolfo, Enel Colombia Extern <gaospina@indracompany.com>
2021-01-15 19:36:44 -0500

    Agregar archivos de proyecto.

---
+++ Helpers/Configuration/RouletteDatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bettingRouletteAPI.Helpers.Configuration
+{
+    public class RouletteDatabaseSettings : IRouletteDatabaseSettings
+    {
+        public string TokensCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IRouletteDatabaseSettings
+    {
+        string TokensCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
